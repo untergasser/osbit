6b8e6d99dd145c17e4ed34d66e75903cf7a3e492
Mathias Gebhardt (NB) <gebhardtmathias@aol.com>
2012-11-08 15:53:44 +0100

    Merged TODO.txt with GitHub issue tracker; removed symlink.txt; updated copyright notice in admin folder

--- admin/controller.php
+++ admin/controller.php
@@ -1,1 +1,41 @@
-<?php/** * @version 1.0 $Id: controller.php 2 2012-10-05 16:49:19Z mathias_lt $ * @package Joomla * @subpackage OSBIT * @copyright (C) 2010 - 2011 Mathias Gebhardt * @license GNU/GPL, see LICENSE.php * OSBIT is free software; you can redistribute it and/or * modify it under the terms of the GNU General Public License 2 * as published by the Free Software Foundation. * OSBIT is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the * GNU General Public License for more details. * You should have received a copy of the GNU General Public License * along with OSBIT; if not, write to the Free Software * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */// No direct accessdefined( '_JEXEC' ) or die( 'Restricted access' );jimport('joomla.application.component.controller'); /** * OSBIT Component Controller * * @package    Joomla * @subpackage OSBIT */class OSBITController extends JController{    /**     * Method to display the view     *     * @access    public     */    public function display($cachable = false, $urlparams = false)	{		// set default view if not set		JRequest::setVar('view', ($view = JRequest::getCmd('view', 'Main')));		 		// call parent behavior		parent::display($cachable, $urlparams);				// Set the submenu		if ($view != 'Main')			OSBITHelper::addSubmenu($view);	}}
+﻿<?php
+/**
+ * @version		1.0.0
+ * @package		Joomla
+ * @subpackage	OSBIT
+ * @copyright	(C) 2010 - 2012 Mathias Gebhardt
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+
+// No direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+ 
+/**
+ * OSBIT Component Controller
+ *
+ * @package    Joomla
+ * @subpackage OSBIT
+ */
+class OSBITController extends JController
+{
+    /**
+     * Method to display the view
+     *
+     * @access    public
+     */
+    public function display($cachable = false, $urlparams = false)
+	{
+		// set default view if not set
+		JRequest::setVar('view', ($view = JRequest::getCmd('view', 'Main')));
+		 
+		// call parent behavior
+		parent::display($cachable, $urlparams);
+		
+		// Set the submenu
+		if ($view != 'Main')
+			OSBITHelper::addSubmenu($view);
+	}
+}
